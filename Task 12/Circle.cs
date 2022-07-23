00ce0c8de7038bf3deb02c9dc8fcd2acbe86d521
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-24 00:19:53 +0300

    Добавьте файлы проекта.

---
+++ Task 12/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2*Math.PI*r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r*r;
+        }
+        static public bool GetAppertain(bool answer, double r, double x0, double y0, double x, double y)
+        {
+            double r1 = Math.Sqrt(Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2));
+            return answer=r1== r;
+            //    if (Math.Sqrt(Math.Pow(2, (x - x0)) + Math.Pow(2, (y - y0))) == r)
+            //{
+                
+            //    return answer = 1; 
+            //    Console.WriteLine("Точка принадлежит окружности");
+            //}
+            //else
+            //{
+            //    return answer = 0; 
+            //    Console.WriteLine("Точка не принадлежит окружности");
+            //}
+                
+        }
+    }
+}
---
+++ Task 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            bool answer=true;
+            double square = Circle.GetSquare(r);
+            double lenght = Circle.GetLength(r);
+            Console.WriteLine("Площадь= {0} \nДлина окружности={1}", square,lenght);
+            Console.WriteLine("Введите координату x0 центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату y0 центра окружности");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату x проверяемой точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координату y проверяемой точки");
+            double y = Convert.ToDouble(Console.ReadLine());
+            bool app = Circle.GetAppertain(answer, r, x0, y0, x, y);
+            Console.WriteLine(app? "Точка принадлежит окружности" : "Точка не принадлежит окружности");
+            Console.ReadKey();
+        }
+    }
+}
